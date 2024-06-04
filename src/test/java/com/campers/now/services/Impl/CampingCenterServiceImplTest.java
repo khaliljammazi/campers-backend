8c1ebc8cd9b9d3fc22cbe87ca508fa1d04d45c1b
khalil-jammazi <54208444+khaliljammazi@users.noreply.github.com>
2024-06-04 21:13:44 +0100

    add metrics

---
+++ src/test/java/com/campers/now/services/Impl/CampingCenterServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.campers.now.services.Impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CampingCenterServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void addActivitybyCampingcenterId() {
+    }
+
+    @Test
+    void calculateOccupancyRate() {
+    }
+
+    @Test
+    void calculateADR() {
+    }
+
+    @Test
+    void calculateRevenuePerOccupiedSpace() {
+    }
+}
